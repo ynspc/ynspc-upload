817bbce10bf2e88e89914c63b027d37f5cbbe0c9
YNSPC <yshrestha007@gmail.com>
2023-03-07 20:30:08 +0545

    updated lib

---
+++ .changeset/few-ways-ring.md
@@ -0,0 +1,5 @@
+---
+"@ynspc/image-upload": patch
+---
+
+new function getName added.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const testOne = () => {
     console.log("Hi this is first test in the ci.")
 }
+
+export const getName = (name: string) => {
+    return `My name is ${name}`
+}
